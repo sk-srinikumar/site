17ad1c235b0d3fb15f6a3882dc65849715205900
Srini Kumar <srnkmrsn@gmail.com>
2021-02-27 21:35:20 -0800

    Material ui plugin added

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const path = require('path')
 
 module.exports = {
   siteMetadata: {
-    title: 'Srini Kumar',
     description:
       'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
     author: '@chrisbiscardi',
   },
   plugins: [
+    `gatsby-plugin-material-ui`,
 
     {
       resolve: `gatsby-plugin-emotion`,
