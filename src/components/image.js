8acf659c7a20c17b1fc3d25ac849bd2d31bd8907
Srini Kumar <srnkmrsn@gmail.com>
2021-02-28 00:44:29 -0800

    Optimizations

--- src/components/image.js
+++ src/components/image.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
-import PropTypes from 'prop-types'
 
 /*
  * This component is built using `gatsby-image` to automatically serve optimized
