1e9580a796bd43b1b2b413d2bc36971a91f0841e
Srini Kumar <srnkmrsn@gmail.com>
2021-02-27 21:15:11 -0800

    Added style plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,12 +9,22 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        // Accepts the following options, all of which are defined by `@emotion/babel-plugin` plugin.
+        // The values for each key in this example are the defaults the plugin uses.
+        sourceMap: true,
+        autoLabel: "dev-only",
+        labelFormat: `[local]`,
+        cssPropOptimization: true,
+      },
+    },
+    {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         displayName: false
       },
     },
-
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
